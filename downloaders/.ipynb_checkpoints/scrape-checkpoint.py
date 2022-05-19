bd3f01a9aac78ce9671913cc83b37f07e958ce57
Sean Greene <seangreene89@gmail.com>
2022-05-19 11:30:43 -0700

    think it works now

--- downloaders/.ipynb_checkpoints/scrape-checkpoint.py
+++ downloaders/.ipynb_checkpoints/scrape-checkpoint.py
@@ -2,12 +2,12 @@
 Download the California State Water Resources Control Board's monthly reports of water production and conservation by the larger urban water suppliers in California.
 Source: https://data.ca.gov/dataset/drinking-water-public-water-system-operations-monthly-water-production-and-conservation-information
 """
-import pathlib
+# import pathlib
 import pandas as pd
 
 # Pathing
-THIS_DIR = pathlib.Path(__file__).parent.absolute()
-DATA_DIR = THIS_DIR / "data"
+# THIS_DIR = pathlib.Path(__file__).parent.absolute()
+# DATA_DIR = THIS_DIR / "data"
 
 
 def main():
@@ -18,7 +18,7 @@ def main():
     url = "https://data.ca.gov/dataset/e4b31aa4-0a61-4e03-84bd-2ae46183db59/resource/0c231d4c-1ea7-43c5-a041-a3a6b02bac5e/download/uw_supplier_data051022.csv"
     df = pd.read_csv(url)
     # Save it to the data folder
-    df.to_csv(DATA_DIR / "raw/water-use-by-district-timeseries.csv", index=False)
+    df.to_csv("data/raw/water-use-by-district-timeseries.csv", index=False)
 
 
 if __name__ == '__main__':
