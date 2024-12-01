dab865aebeaf03eaf1055f5d409089b2c48b2f19
Sean Greene <seangreene89@gmail.com>
2024-12-01 15:18:52 -0800

    fix scraper

--- urban-water-use/.ipynb_checkpoints/scrape-checkpoint.py
+++ urban-water-use/.ipynb_checkpoints/scrape-checkpoint.py
@@ -5,6 +5,7 @@ Source: https://data.ca.gov/dataset/urws-conservation-supply-demand
 """
 import pathlib
 import pandas as pd
+import requests as re
 
 # Pathing
 THIS_DIR = pathlib.Path(__file__).parent.absolute()
@@ -13,12 +14,24 @@ DATA_DIR = THIS_DIR / "data"
 
 def main():
     """
-    Download the Tableau export as a CSV.
+    Download and export as CSV
     """
-    # Download the data
-    url = "https://data.ca.gov/dataset/c69ac02b-adfb-459a-bc58-bf69a8b572d2/resource/f4d50112-5fb5-4066-b45c-44696b10a49e/download/monthly_combined_dataset.csv"
-    df = pd.read_csv(url)
+    
+    # Request data from URL
+    url = "https://data.chhs.ca.gov/dataset/e283ee5a-cf18-4f20-a92c-ee94a2866ccd/resource/130d7ba2-b6eb-438d-a412-741bde207e1c/download/covid19vaccinesbycounty.csv"
+    headers={'User-Agent': 'Mozilla/5.0'}
+    response = re.get(url, headers=headers)
+    response_str = response.text
+    
+    # Parse the response string to dataframe
+    df = pd.DataFrame([row.split(',') for row in response_str.split('\r\n')])
+    # Dataframe will have column names as first row
+    # move them to column headers
+    df.columns = df.iloc[0]  # Set the first row as column names
+    df = df[1:] 
+    # lowercase headers
     df.columns = map(str.lower, df.columns)
+    
     # Save it to the data folder
     df.to_csv(DATA_DIR / "latest.csv", index=False)
 
